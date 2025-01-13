326c4c3b7ce200898250427afe60772f05ff93cf
Timo Warrinnier <timo.warrinier@gmail.com>
2025-01-13 08:32:24 +0100

    Uncommented alert sending

--- functions/index.js
+++ functions/index.js
@@ -7,20 +7,20 @@ const { sendAlert } = require("./alert");
 exports.downtimeMeter = onSchedule("every 5 mins", async (event) => {
     try {
         // Replace with your Strapi health check endpoint
-        const response = await axios.get('https://a855-2a02-a03f-e02e-e701-a5fc-94bf-e658-1601.ngrok-free.app/api/customers');
+        const response = await axios.get('https://brave-deer-edfafb83b2.strapiapp.com/api/deliveries');
 
         logger.info('Response', response);
 
         if (response.status === 200) {
-            //sendAlert('Strapi is up and running');
+            sendAlert('Strapi is up and running');
             logger.info('Strapi is up and running');
         } else {
-            //sendAlert('Strapi is down');
+            sendAlert('Strapi is down');
             logger.error('Strapi is down');
             // Optionally, send an alert (e.g., via email or a messaging service)
         }
       } catch (error) {
-        //sendAlert('Strapi is down');
+        sendAlert('Strapi is down');
         logger.error('Strapi is down: ', error);
         // Optionally, send an alert (e.g., via email or a messaging service)
       }
